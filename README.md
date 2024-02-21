61de77b0a873ada91e20abcf17db40a2ea59e64f
Cláudio Schirmer Guedes <claudioschirmer@icloud.com>
2024-02-21 14:08:02 -0500

    readme

---
+++ README.md
@@ -0,0 +1,52 @@
+# Utils-Kotlin
+utils-kotlin is a Kotlin library for fast kotlin development
+
+>Package: **br.dev.schirmer.utils.kotlin**<br/>
+>Developer: **Claudio Schirmer Guedes**
+
+## Statements
+
+### Guard
+```kotlin
+guard(condition: Boolean, onFailure: () -> Nothing)
+```
+
+## Extensions
+
+### from Any
+```kotlin
+// Json
+TObject.toJson<TObject>(
+    alphabeticalOrder: Boolean = true,
+    exportOptionsInclude: ExportOptionsInclude = ExportOptionsInclude.NON_EMPTY
+) : String
+```
+
+### from String
+
+#### Json
+```kotlin
+String.toClass<TObject>(): TObject
+```
+
+#### Cryptography
+```kotlin
+String.encrypt(key: String): String
+
+String.decrypt(key: String): String
+
+String.encryptURL(key: String): String
+
+String.decryptURL(key: String): String
+
+String.encryptHash(): String
+
+String.equalsHash(hash: String): Boolean
+```
+
+#### Transformations
+```kotlin
+String.onlyDigits<TResult>(): TResult
+
+String.removeDiacritics(): String
+```
--- src/br/dev/schirmer/utils/kotlin/string/Extensions.kt
+++ src/br/dev/schirmer/utils/kotlin/string/Extensions.kt
@@ -1,32 +1,16 @@
 package br.dev.schirmer.utils.kotlin.string
 
-import com.fasterxml.jackson.databind.DeserializationFeature
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
-import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
-import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import java.text.Normalizer
 
-inline fun <reified T : Any> String.onlyDigits(): T = this.filter { it.isDigit() }.let {
-    return when (T::class) {
-        String::class -> it as T
-        Int::class -> (if(it.isNotBlank()) it.toInt() else 0) as T
-        Long::class -> (if(it.isNotBlank()) it.toLong() else 0L) as T
-        Double::class -> (if (it.isNotBlank()) it.toDouble() else 0.0) as T
+inline fun <reified TResult : Any> String.onlyDigits(): TResult = this.filter { it.isDigit() }.let {
+    return when (TResult::class) {
+        String::class -> it as TResult
+        Int::class -> (if(it.isNotBlank()) it.toInt() else 0) as TResult
+        Long::class -> (if(it.isNotBlank()) it.toLong() else 0L) as TResult
+        Double::class -> (if (it.isNotBlank()) it.toDouble() else 0.0) as TResult
         else -> throw Throwable("String.onlyDigits() only supports return types such as String, Int, Long, or Double!")
     }
 }
 
 fun String.removeDiacritics(): String =
     Regex("\\p{InCombiningDiacriticalMarks}+").replace(Normalizer.normalize(this, Normalizer.Form.NFD), "")
-
-inline fun <reified TObject : Any> String.toClass(): TObject {
-    return try {
-        jacksonObjectMapper()
-            .registerKotlinModule()
-            .registerModule(JavaTimeModule())
-            .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
-            .readValue(this, TObject::class.java)
-    } catch (e: Exception) {
-        throw Exception("Json cannot be converted to class.", e)
-    }
-}
